6caf092e5cb6380da52ff08751d9811de3a8eb16
Hiteshmeher <meher.hitesh4@gmail.com>
2022-06-29 15:30:17 +0530

    swayam 222

--- src/demoswayamm/empo.java
+++ src/demoswayamm/empo.java
@@ -2,14 +2,23 @@ package demoswayamm;
 
 public class empo {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Problem");
+
+    static final int WagePerHour = 20;
+    static final int FullDayHours = 8;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Problem::");
+        int dailyWages = 0;
         int attendance = (int) Math.floor(Math.random()*10)%3;
         if (attendance == 0)
             System.out.println("Emloyee is absent");
-        else if (attendance == 1)
+        else if (attendance == 1) {
             System.out.println("Employee is present");
-        else
+            dailyWages = WagePerHour * FullDayHours;
+            System.out.println("Daily Wages of Employee: "+dailyWages);
+        }
+        else {
             System.out.println("Employee is present for the Half day");
+        }
     }
 }
